ce82f33ed71087c350497fd3bd8f7625363bcc22
Vinicius <vinicius.souza@smartsupport.com.br>
2022-04-10 22:45:09 -0300

    start apps of project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TicketModule } from './ticket/ticket.module';
+import { UsersModule } from './users/users.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { LocationsModule } from './locations/locations.module';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TicketModule, UsersModule, TicketsModule, LocationsModule, CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  email: string;
+  username: string;
+  password: string;
+  role: string;
+}
