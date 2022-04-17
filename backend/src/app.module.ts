178f6c01b85a08ff92d9d2d762dc011378de7125
vinicius <nascimento.vinicius32@gmail.com>
2022-04-16 22:52:46 -0300

    start projecc module companies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/users/users.module';
+import { CompaniesModule } from './modules/companies/companies.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, CompaniesModule],
 })
 export class AppModule {}
